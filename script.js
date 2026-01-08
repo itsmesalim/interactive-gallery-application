6b55950f0bda5524a8b59c5d1c11a7f0cb66241d
Salim Ullah <itsme.salimullah@gmail.com>
2026-01-08 12:04:45 +0500

    commit for swipping image through figer

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let viewImage = document.querySelector(".view-image");
 let fullImage = document.querySelector(".view-image img");
 
 let currentIndex = 0;
+let touchStartX = 0;
+let touchEndX = 0;
 
 images.forEach((image, index) => {
   image.addEventListener("click", (e) => {
@@ -33,4 +35,25 @@ const previousFunction = () => {
 
 const closedFunction = () => {
   viewImage.style.display = "none";
+};
+
+fullImage.addEventListener("touchstart", (e) => {
+  touchStartX = e.changedTouches[0].screenX;
+});
+
+fullImage.addEventListener("touchend", (e) => {
+  touchEndX = e.changedTouches[0].screenX;
+  handleSwipe();
+});
+
+const handleSwipe = () => {
+  const swipeThreshold = 50;
+
+  if (touchEndX < touchStartX - swipeThreshold) {
+    nextFunction();
+  }
+
+  if (touchEndX > touchStartX + swipeThreshold) {
+    previousFunction();
+  }
 };
